0abdae442332c56f3cea8991e5d276c963c2a35a
Austin Akers <austin.akers5@gmail.com>
2019-03-04 17:46:20 -0600

    3 solutions for a reversed string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //My First Solution
+    // return str.split('').reverse().join('');
+   
+    //My Second Solution
+    // let reversedArray = '';
+    // for( let character of str){
+    //     reversedArray = character + reversed;
+    // }
+    // return reversedArray;
+
+    //My Third Solution
+    str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '')
+}
 
 module.exports = reverse;
