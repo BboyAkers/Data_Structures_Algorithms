c62ffc09c042317819bfc9928ebfb87e5fdc386d
Austin Akers <austin.akers5@gmail.com>
2019-04-03 22:37:34 -0500

    pushing breathe sort and depth sort

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,50 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+    add(data){
+        this.children.push(new Node(data));
+    }
+    remove(data) {
+        this.children.filter(node => {
+            return node.data !== data; 
+        });
+    }
+}
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+          
+    }
+    traverseBF(fn){
+        //creating top node
+        const arr = [this.root];
+        // pushing node inside array and while it has something in it
+        while(arr.length){
+            //Take out first element of array
+            const node = arr.shift();
+            // Take all the elements out of node and push them into the back of the array
+            arr.push(...node.children)
+            fn(node);
+        }
+    }
+    traverseDF(fn){
+        //creating top node
+        const arr = [this.root];
+        // pushing node inside array and while it has something in it
+        while(arr.length){
+            //Take out first element of array
+            const node = arr.shift();
+            // Take all the elements out of node and push them into the beginning of the array
+            arr.unshift(...node.children)
+            fn(node);
+        }
+    }
+}
 
 module.exports = { Tree, Node };
